a334e1c9aaac7a67e7309d369cce10d4ef30141e
Rafael Bernardes Sposito <rajingg7@gmail.com>
2026-08-13 20:49:16 -0300

    Primeiro commit

---
+++ app/index.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity
+}
+    from 'react-native';
+//components
+import AppInput from '../components/AppInput.js';
+import AppButton from '../components/AppButton.js';
+
+export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    return (
+        <View style={styles.container}>
+            <Text>Meu Bolso</Text>
+            <Text>Controle suas finanças</Text>
+            <AppInput label="Email" placeholder="Digite seu email" autoCapitalize="none" keyboardType="email-address"
+            value={email} onChangeText={setEmail} />
+            <AppInput label="Senha" secureTextEntry value={password} onChangeText={setPassword} placeholder="Digite sua senha"/>
+            <AppButton title="Entrar"  loading={loading} />
+            <TouchableOpacity>
+                <Text>Criar nova conta</Text>
+            </TouchableOpacity>
+        </View> 
+    );
+}
+
+const styles = StyleSheetreate({
+
+});
---
+++ src/components/AppButton.js
@@ -0,0 +1,38 @@
+import {
+    ActivityIndicator,
+    Text,
+    TouchableOpacity,
+    StyleSheet
+} from "react-native";
+import { COLORS, RADIUS, SPACING } from '../constants/Theme.js';
+
+export default function AppButton({ title, onPress, loading = false, disabled = false }) {
+    return (
+        <TouchableOpacity
+            style={[styles.button, (disabled || loading)
+                && styles.disabled]}
+            onPress={onPress}
+            disabled={disabled || loading}>
+            {loading ?
+                <ActivityIndicator color="#fff" /> :
+                <Text style={styles.title}>{title}</Text>}
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: COLORS.primary,
+        padding: SPACING.md,
+        borderRadius: RADIUS.md,
+        alignItems: 'center',
+    },
+    disabled: {
+        opacity: 6
+    },
+    text: {
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: '700'
+    }
+})
