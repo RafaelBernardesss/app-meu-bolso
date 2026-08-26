01bd3a11d86a8f176a68d48ab569371585e387b8
Rafael Bernardes Sposito <rajingg7@gmail.com>
2026-08-25 21:06:04 -0300

    Finalizando o registro

--- app/RegisterScreen.js
+++ app/RegisterScreen.js
@@ -10,6 +10,7 @@ import {
 } from 'react-native';
 import Appinput from '../src/components/AppInput';
 import AppButton from '../src/components/AppButton';
+import {useRouter} from 'expo-router';
 
 
 
@@ -21,6 +22,7 @@ export default function Register(){
     const [senha, setSenha] = useState('');
     const [loading, setLoafing] = useState(false)
 
+    const router = useRouter()
 
 
     return(
@@ -33,13 +35,39 @@ export default function Register(){
                 value={nome} onChangeText={setNome}/>
                 <Appinput label="Email" placeholder="Digite seu email" autoCapitalize="none" keyboardType="email-address"
                 value={email} onChangeText={setEmail}/>
-             </View>
                 <Appinput label="Senha" secureTextEntry value={senha} onChnage={setSenha} placeholder="Digite sua senha"/>
+                <AppButton title="Registrar"/>
+                <TouchableOpacity>
+                    <Text style={styles.link} onPress={() => router.push('/index')}>Já tem uma conta?</Text>
+                </TouchableOpacity>
+             </View>
         </KeyboardAvoidingView>
        
     );
 }
 
 const styles = StyleSheet.create({
-
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 24,
+    },
+    title:{
+        fontSize: 34,
+        fontWeight: '900',
+        color: '#2f3640',
+        textAlign: 'center'
+    },
+    subtitle:{
+        color: '#7f8c8d',
+        textAlign: 'center',
+        marginTop: 8,
+        marginBottom: 32
+    },
+    link:{
+        color: '#008f22',
+        textAlign: 'center',
+        marginTop: 20,
+        fontWeight: '700'
+    },
 });
